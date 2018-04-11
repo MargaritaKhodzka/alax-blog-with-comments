1620b778c9c8bb13ceb5be3501df865a346408c9
MargaritaKhodzka <mkhodzka@gmail.com>
2018-04-11 01:01:50 -0400

    load comments

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,39 @@
-$('a.load_comments').on('click', function (e) {
-  e.preventDefault()
-})
+// $(function() {
+
+  // $('a.load_comments').on('click', function(e){
+  //   $.ajax({
+  //     url: this.href,
+  //     dataType: 'script'
+  //   })
+  //
+  //   e.preventDefault()
+  // })
+  // $('a.load_comments').on('click', function (e) {
+    // $.ajax({
+    //   method: 'GET',
+    //   url: this.href
+    // }).done(function(response) {
+    //   //get the response, its the variable data
+    //   $('div.comments').append(response)
+    // })
+
+    // // requesting html
+    // $.get(this.href).done(function(response) {
+    //   $('div.comments').html(response)
+    // })
+
+    // //requesting json
+    // $.get(this.href).done(function(json) {
+    //   //clear the ol html in case there were stale comments
+    //   var $ol = $('div.comments ol')
+    //   $ol.html('')
+    //   //iterate over each comment within json
+    //   //append an li to the ol
+    //   json.forEach(function(comment){
+    //     // with each comment data, append an LI to the OL with the comment content
+    //     $ol.append("<li>" + comment.content + "</li>");
+    //   })
+    // })
+    // e.preventDefault()
+  // })
+// })
